6b585a6d82c6c564c4b7e37ff449cd8e907f0241
Yash Khati <yashkhati23@gmail.com>
2024-08-06 21:05:35 +0530

    Created Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Signin from './pages/SignIn'
+import Header from './components/Header'
 function App() 
 {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element ={<Home/>} />
         <Route path='/about' element ={<About/>} />
