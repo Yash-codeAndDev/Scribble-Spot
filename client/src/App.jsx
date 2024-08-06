a92dec29157e3f2f95cf6dcd9c37123ca73584ca
Yash Khati <yashkhati23@gmail.com>
2024-08-06 12:57:05 +0530

    Created pages templates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-function App() {
-
+import Home from './pages/Home'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/SignIn'
+function App() 
+{
   return (
-    <>
-      <h1>Blogs For Geeks</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Home/>} />
+        <Route path='/about' element ={<About/>} />
+        <Route path='/sign-up' element ={<SignUp/>} />
+        <Route path='/sign-in' element ={<Signin/>} />
+        <Route path='/dashbord' element ={<Dashboard/>} />
+        <Route path='/projects' element ={<Projects/>} />
+       
+      </Routes>
+    </BrowserRouter>
   )
 }
 
